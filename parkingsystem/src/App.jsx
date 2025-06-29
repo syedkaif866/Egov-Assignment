e9c51ec292f8ce6390921571df69c553a020fa4d
syedkaif866 <syedkaif866@gmail.com>
2025-06-29 15:07:23 +0530

    initial commit setup

---
+++ parkingsystem/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+     <h1 class="text-3xl font-bold underline">
+    Hello world!
+    </h1>
+  )
+}
+
+export default App
